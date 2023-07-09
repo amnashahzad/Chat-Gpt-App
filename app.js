35afbee271074e59c7c648e7b70918c959c0beef
Amna Shahzad <121329321+amnashahzad@users.noreply.github.com>
2023-07-09 07:23:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
+
+let userText = null;
+
+const loadDataFromLocalstorage = () => {
+  const themeColor = localStorage.getItem("themeColor");
+
+  document.body.classList.toggle("light-mode", themeColor === "light_mode");
+  themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+
+  const defaultText = `<div class="default-text">
+                          <h1>ChatGPT Clone</h1>
+                          <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+                      </div>`;
+
+  chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+};
+
+const createChatElement = (content, className) => {
+  const chatDiv = document.createElement("div");
+  chatDiv.classList.add("chat", className);
+  chatDiv.innerHTML = content;
+  return chatDiv;
+};
+
+const getChatResponse = () => {
+  // Define your predefined responses here
+  const predefinedResponses = {
+    hello: "Hello there!",
+    how_are_you: "I'm doing well, thank you!",
+    default: "I'm sorry, I didn't understand that. Can you please rephrase?",
+  };
+
+  const userMessage = userText.toLowerCase().trim();
+  const responseText = predefinedResponses[userMessage] || predefinedResponses.default;
+
+  const html = `<div class="chat-content">
+                  <div class="chat-details">
+                      <img src="img/chatbot.png" alt="chatbot-img">
+                      <p>${responseText}</p>
+                  </div>
+              </div>`;
+
+  const incomingChatDiv = createChatElement(html, "incoming");
+  chatContainer.appendChild(incomingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  localStorage.setItem("all-chats", chatContainer.innerHTML);
+};
+
+const handleOutgoingChat = () => {
+  userText = chatInput.value.trim();
+  if (!userText) return;
+
+  chatInput.value = "";
+  chatInput.style.height = `${initialInputHeight}px`;
+
+  const html = `<div class="chat-content">
+                  <div class="chat-details">
+                      <img src="img/amna.png" alt="user-img">
+                      <p>${userText}</p>
+                  </div>
+              </div>`;
+
+  const outgoingChatDiv = createChatElement(html, "outgoing");
+  chatContainer.querySelector(".default-text")?.remove();
+  chatContainer.appendChild(outgoingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  setTimeout(getChatResponse, 500);
+};
+
+deleteButton.addEventListener("click", () => {
+  if (confirm("Are you sure you want to delete all the chats?")) {
+    localStorage.removeItem("all-chats");
+    loadDataFromLocalstorage();
+  }
+});
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+  localStorage.setItem("themeColor", themeButton.innerText);
+  themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {
+  chatInput.style.height = `${initialInputHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleOutgoingChat();
+  }
+});
+
+loadDataFromLocalstorage();
+sendButton.addEventListener("click", handleOutgoingChat);
